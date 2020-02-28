df063ab731901e51794ede10b3409203364a5436
Badal Khatri <badal.k@solutelabs.com>
2020-02-28 18:04:42 +0530

    first commit

---
+++ src/pages/login.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useApolloClient, useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+import { LoginForm, Loading } from "../components";
+import ApolloClient from "apollo-client";
+import * as loginTypes from "./__generated__/login";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!) {
+    login(email: $email)
+  }
+`;
+
+export default function Login() {
+  const client: ApolloClient<any> = useApolloClient();
+
+  const [login, { loading, error }] = useMutation<
+    loginTypes.login,
+    loginTypes.loginVariables
+  >(LOGIN_USER, {
+    onCompleted({ login }) {
+      localStorage.setItem("token", login as string);
+      client.writeData({ data: { isLoggedIn: true } });
+    }
+  });
+
+  if (loading) return <Loading />;
+  if (error) return <p>An error occured</p>;
+
+  return <LoginForm login={login} />;
+}
